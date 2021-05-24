9eecad59080c16cb2d70796bcf95697266096fb0
Muhammad Zainul Ahda <zainahda@gmail.com>
2021-05-24 11:39:05 +0700

    creating card users data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- header section -->
+    <div class="header text-center mt-5 mb-5">
+      <h1>Users Card</h1>
+    </div>
+
+    <div class="card-wrap d-flex justify-content-center">
+      <!-- card section -->
+      <div class="container row row-cols-md-3">
+        <!-- looping data from axios -->
+        <div v-for="user in users" :key="user">
+          <!-- card data start -->
+          <b-card
+            :img-src="user.picture.large"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem"
+            class="card mr-3 mb-5"
+          >
+            <b-card-text>
+              <p>
+                Name:
+                <strong>{{ user.name.first }} {{ user.name.last }}</strong>
+              </p>
+              <a :href="user.email">Email address: {{ user.email }}</a>
+              <p>
+                Telephone: {{ user.phone }}
+                <br />
+                Mobile: {{ user.cell }}
+                <br />
+                Country: {{ user.location.country }}
+              </p>
+            </b-card-text>
+          </b-card>
+          <!-- card data end -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      users: "",
+    };
+  },
+
+  created() {
+    axios
+      .get("https://randomuser.me/api/?results=20")
+      .then((response) => (this.users = response.data.results));
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
