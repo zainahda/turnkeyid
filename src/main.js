503f967d8820cfe9a98f26f540d320dc3d44c046
Muhammad Zainul Ahda <zainahda@gmail.com>
2021-05-24 11:38:17 +0700

    config bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
